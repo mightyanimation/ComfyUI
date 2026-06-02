2a17cb57c5de9f96b8a97907a0cb209355802427
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2026-06-01 18:00:07 -0600

    prevent the use of CUDA

--- comfy/model_management.py
+++ comfy/model_management.py
@@ -204,7 +204,8 @@ def get_torch_device():
         elif is_mlu():
             return torch.device("mlu", torch.mlu.current_device())
         else:
-            return torch.device(torch.cuda.current_device())
+            # return torch.device(torch.cuda.current_device())
+            return torch.device("cpu")
 
 def get_total_memory(dev=None, torch_total_too=False):
     global directml_enabled
